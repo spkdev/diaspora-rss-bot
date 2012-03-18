13a7e6bf1299c3da44006bcb7e9b372de5e8a3b6
Oliver Nacke <spektrum@fractal.bz>
2012-03-18 21:58:00 +0100

    - Some cleanup - Show feed url instead of item url, so it is easier to recognize the url for a user that wants to unsubscribe

--- diaspora-bot/lib/Diaspora/Client.pm
+++ diaspora-bot/lib/Diaspora/Client.pm
@@ -4,7 +4,6 @@ use strict;
 use warnings;
 use vars qw($VERSION @ISA);
 use Carp;
-
 use LWP::UserAgent;
 use HTML::Entities;
 use URI::Escape;
--- diaspora-bot/lib/Diaspora/RSSBot.pm
+++ diaspora-bot/lib/Diaspora/RSSBot.pm
@@ -5,11 +5,13 @@ use warnings;
 use vars qw($VERSION @ISA);
 use Diaspora::Client;
 use Carp;
-use JSON;
 use Switch;
 use DBI;
+use XML::Feed;
+use Digest::MD5 qw(md5 md5_hex md5_base64);
 use List::MoreUtils 'any';
-
+use Encode;
+use utf8;
 
 our $VERSION = '0.01';
 our %flags   = qw(pod 1 user 1 passwd 1);
@@ -33,12 +35,12 @@ sub init
   my $self = shift;
   my %args = @_;
 
-  if( !exists $args{pod}    ) { croak "Parameter 'pod' is missing";   }
-  if( !exists $args{user}   ) { croak "Parameter 'user' is missing";  }
-  if( !exists $args{passwd} ) { croak "Parameter 'passwd' is missing";}
-  if( !exists $args{db}     ) { croak "Parameter 'db' is missing";    }
-  $self->{dbh}      = undef;
-  $self->{db}       = $args{db};
+  if( !exists $args{pod}     )  { croak "Parameter 'pod' is missing";     }
+  if( !exists $args{user}    )  { croak "Parameter 'user' is missing";    }
+  if( !exists $args{passwd}  )  { croak "Parameter 'passwd' is missing";  }
+  if( !exists $args{db_path} )  { croak "Parameter 'db_path' is missing"; }
+  $self->{db}       = undef;
+  $self->{db_path}  = $args{db_path};
   $self->{diaspora} = Diaspora::Client->new( pod => $args{pod}, user => $args{user}, passwd => $args{passwd} );
   $self->_db_prepare();
   return $self;
@@ -68,7 +70,7 @@ sub process_user_tasks
       case "help"         { $self->_handle_help( $_ );        }
       case "subscribe"    { $self->_handle_subscribe( $_ );   }
       case "unsubscribe"  { $self->_handle_unsubscribe( $_ ); }
-#      case "list"         { $self->_handle_list( $_ );        }      
+      case "list"         { $self->_handle_list( $_ );        }      
       else                { $self->_handle_invalid( $_ );     }
     }
   }
@@ -76,10 +78,63 @@ sub process_user_tasks
 
 sub process_feeds
 {
-  my $self = shift;
-  my $pm = shift;
+  my $self    = shift;
+  my $pm      = shift;
+  my $aspect  = undef;
+  
+  my $st_get_feed = $self->{db}->prepare( "SELECT * FROM feeds;" );
+  my $st_get_processed = $self->{db}->prepare( "SELECT * FROM processed WHERE guid=?;" );
+  my @aspects = $self->{diaspora}->get_aspects();
 
-  print "Processing feeds ...\n";
+  $st_get_feed->execute();
+  while( my $fi = $st_get_feed->fetchrow_hashref() )
+  {
+    # Find corresponding aspect
+    foreach ( @aspects )
+    {
+      if( $_->{name} eq $fi->{guid} )
+      {
+        $aspect = $_;
+        last;
+      }
+    }
+
+    if( $aspect )
+    {
+      eval
+      {
+        my $feed = XML::Feed->parse( URI->new( $fi->{url} ) );
+
+        foreach ( $feed->entries )
+        {
+          my $content   = (defined $_->content->body) ? $_->content->body : (defined $_->summary->body) ? $_->summary->body : "";
+          my $feed_url  = utf8::is_utf8($fi->{url}) ? encode('utf8', $fi->{url}) : $fi->{url};
+          my $link      = utf8::is_utf8($_->link)   ? encode('utf8', $_->link)   : $_->link;
+          my $title     = utf8::is_utf8($_->title)  ? encode('utf8', $_->title)  : $_->title;
+          $content      = utf8::is_utf8($content)   ? encode('utf8', $content)   : $content;
+
+          my $guid = md5_hex( $title.$content ); # This to get _every_ update of the post, or use url to only get it once?!
+          $st_get_processed->execute( $guid );
+          if( !$st_get_processed->fetchrow_hashref() )
+          {
+            print "NEW ITEM [$fi->{url} -> \"$title\" aspects: \"$aspect->{name}\", $aspect->{aspect_id}]\n";
+
+            my $message = $self->_format_message( $feed_url, $link, $title, $content );
+            $self->{diaspora}->post_message( $message, [$aspect->{aspect_id}] );
+            $self->{db}->do( "INSERT INTO processed VALUES( $fi->{guid}, \"$guid\", datetime('now') );" );
+          }
+        }
+        1;
+      }
+      or do
+      {
+        print "ERROR while processing $fi->{url} -> Processing next feed\n";
+        next;
+      }
+    }  
+  }
+
+# TODO: Purge old feeds
 }
 
 sub _handle_help
@@ -102,8 +157,8 @@ sub _handle_subscribe
   my $regex = qr/${\(PATTERN_HREF)}/;
   push @feeds, $1 while $params =~ /$regex/g;
 
-  my $st_add = $self->{dbh}->prepare( "INSERT INTO feeds VALUES( NULL, ? );" );
-  my $st_get = $self->{dbh}->prepare( "SELECT * FROM feeds WHERE url=?;" );
+  my $st_add = $self->{db}->prepare( "INSERT INTO feeds VALUES( NULL, ? );" );
+  my $st_get = $self->{db}->prepare( "SELECT * FROM feeds WHERE url=?;" );
 
   foreach ( @feeds )
   {
@@ -120,7 +175,6 @@ sub _handle_subscribe
         my $aspect_id = $self->{diaspora}->create_aspect( $row->{guid} );
         $self->{diaspora}->add_user_to_aspect( $pm->{from_user_id}, $aspect_id );
         push @added, $_;
-#       print "### Added feed to db, created aspect and added user to aspect\n";
       }
     }
     else
@@ -142,9 +196,9 @@ sub _handle_subscribe
         
         $self->{diaspora}->add_user_to_aspect( $pm->{from_user_id}, $aspect->{aspect_id} );
         push @added, $_;
-#       print "### Added user to existing aspect\n";
       }
     }
+    print "SUBSCRIBE: Added user to feed \"$_\"\n";
   }
   
   my $response = "Request processed, you have been added to the following feeds:\n\n";
@@ -168,8 +222,8 @@ sub _handle_unsubscribe
   my $regex = qr/${\(PATTERN_HREF)}/;
   push @feeds, $1 while $params =~ /$regex/g;
 
-  my $st_get = $self->{dbh}->prepare( "SELECT * FROM feeds WHERE url=?;" );
-  my $st_del = $self->{dbh}->prepare( "DELETE FROM feeds WHERE guid=?;" );
+  my $st_get = $self->{db}->prepare( "SELECT * FROM feeds WHERE url=?;" );
+  my $st_del = $self->{db}->prepare( "DELETE FROM feeds WHERE guid=?;" );
 
   foreach ( @feeds )
   {
@@ -203,7 +257,6 @@ sub _handle_unsubscribe
 
       if( $user_found )
       {
-#        print "MATCH FOUND\n";
         $self->{diaspora}->remove_user_from_aspect( $pm->{from_user_id}, $aspect->{aspect_id} );
  
         if( ($#users_in_aspect + 1) == 1 )
@@ -215,16 +268,15 @@ sub _handle_unsubscribe
           }
         }
         push @removed, $_;
+        print "UNSUBSCRIBE: Removed user from feed \"$_\"\n";
       }
       else
       {
-#        print "NO MATCH FOUND\n";
         push @error, $_;
       }
     }
     else
     {
-#      print "NO MATCH FOUND, no db entry\n";
       push @error, $_;
     }
   }
@@ -245,63 +297,44 @@ sub _handle_unsubscribe
   $self->{diaspora}->delete_conversation( $pm->{conversation_id} );
 }
 
+sub _handle_list
+{
+  my $self = shift;
+  my $pm = shift;
+ 
+  $self->{diaspora}->reply_conversation( $pm->{conversation_id}, "Not yet implemented" );
+  $self->{diaspora}->delete_conversation( $pm->{conversation_id} );
+}
+
 sub _handle_invalid
 {
   my $self = shift;
   my $pm = shift;
 
   my $message = "**Your request is invalid, please refer to the help to see what commands are currently supported:**\n\n".HELP_MESSAGE;
-
   $self->{diaspora}->reply_conversation( $pm->{conversation_id}, $message );
   $self->{diaspora}->delete_conversation( $pm->{conversation_id} );
 }
 
+sub _format_message
+{
+  my $self    = shift;
+  my $feed_url= shift;
+  my $link    = shift;
+  my $title   = shift;
+  my $content = shift;
 
-#######################
-
-
-sub postfeed {
-  my $self = shift;
-  my %arg  = @_;
-
-  $self->{diaspora}->post( message => $self->_feed2message(%arg),
-               uri     => '/status_messages' );
+  my $message = sprintf( "### [%s](%s)\r\n", $self->_escape( $self->_strip( $title ) ), $self->_escape( $link ) );
+  $message = $message.sprintf( "[%s](%s)\r\n\r\n", $self->_escape( $feed_url ), $self->_escape( $feed_url ) );
+  $message = $message.$self->{diaspora}->wc->html2wiki( html => $content );
+  return $message;
 }
 
-sub _feed2message {
-  my $self = shift;
-  my %arg  = @_;
-
-  my $text = $self->_link(%arg)."\r\n".$self->_tags(%arg)."\r\n\r\n".$self->{diaspora}->wc->html2wiki( html => $arg{body} );
-
-  return {
-		status_message => {
-			text => $text,
-		},
-		aspect_ids => $arg{aspect}
-  };
-}
-
-sub _link {
+sub _link
+{
   my $self = shift;
   my %arg  = @_;
   return sprintf "### [%s](%s)\r\n", $self->_escape($self->_strip($arg{title})), $self->_escape($arg{link}); 
-}
-
-sub _tags {
-  my $self = shift;
-  my %arg  = @_;
-
-  my $tags;
-
-  if( $arg{tags} ) {
-    $tags = join ' ', map { "#$_" } @{$arg{tags}};   
-  }
-  else {
-    $tags = $self->_tagfromuri(%arg);    
-  }
-
-  return $tags;
 }
 
 sub _escape {
@@ -322,110 +355,20 @@ sub _strip {
   return $string;
 }
 
-sub _tagfromuri {
-  my $self = shift;
-  my %arg  = @_;
-
-  my $hashtag = $arg{uri};
-  
-  for($hashtag)
-  {
-    s/https?:\/\/(.*)\/?$/$1/g; # Cut off protocol
-    s/\/$//;                    # Cut off possible trailing /
-    s/[^a-zA-Z0-9_]/-/g;        # Replace special chars with '-'
-  }
- 
-  return sprintf "#rss-%s #rss-all", $hashtag;
-}
-
-
-####################################
-
-
-
 sub _db_prepare
 {
   my $self = shift;
-  $self->{dbh} = DBI->connect( "dbi:SQLite:$self->{db}" ) or die "Could not connect to sqlite db $self->{db}: $!\n";
+  $self->{db} = DBI->connect( "dbi:SQLite:$self->{db_path}" ) or die "Could not connect to sqlite db $self->{db_path}: $!\n";
   
   # Create table 'feeds'
-  my $sth = $self->{dbh}->prepare( "CREATE TABLE IF NOT EXISTS feeds (guid INTEGER PRIMARY KEY, url varchar(255) NOT NULL UNIQUE);" );
-  $sth->execute() or die "Could not create table 'feeds' for sqlite db $self->{db}: $!\n";
+  my $sth = $self->{db}->prepare( "CREATE TABLE IF NOT EXISTS feeds (guid INTEGER PRIMARY KEY, url varchar(255) NOT NULL UNIQUE);" );
+  $sth->execute() or die "Could not create table 'feeds' for sqlite db $self->{db_path}: $!\n";
 
   # Create table 'processed'
-  $sth = $self->{dbh}->prepare( "CREATE TABLE IF NOT EXISTS processed (feed INTEGER, guid varchar(255) NOT NULL, timestamp DATETIME);" );
-  $sth->execute() or die "Could not create table 'processed' for sqlite db $self->{db}: $!\n";
+  $sth = $self->{db}->prepare( "CREATE TABLE IF NOT EXISTS processed (feed INTEGER, guid varchar(255) NOT NULL, timestamp DATETIME);" );
+  $sth->execute() or die "Could not create table 'processed' for sqlite db $self->{db_path}: $!\n";
 }
 
-sub _db_add_feed
-{
-  my $self  = shift;
-  my $feed  = shift;
-  my $sth   = $self->{dbh}->prepare( "INSERT INTO feeds VALUES( NULL, ? );" );
-  $sth->execute( $feed ) or die "Could not insert ( NULL, $feed ) in table 'feeds': $!\n";
-}
-
-sub _db_del_feed
-{
-  my $self  = shift;
-  my $feed  = shift;
-  my $sth   = $self->{dbh}->prepare( "DELETE FROM feeds WHERE url=$feed;" );
-  $sth->execute() or die "Could not delete $feed from table 'feeds': $!\n";
-}
-
-sub _db_get_feed_guid
-{
-  my $self  = shift;
-  my $feed  = shift;
-  my $sth   = $self->{dbh}->prepare( "INSERT INTO feeds VALUES( NULL, ? );" );
-  $sth->execute( $feed ) or die "Could not insert ( NULL, $feed ) in table 'feeds': $!\n";
-}
-
-sub _db_add_processed
-{
-  my $self  = shift;
-  my $feed  = shift;
-  my $guid  = shift;
-  my $sth   = $self->{dbh}->prepare( "INSERT INTO processed VALUES( ?, ? );" );
-  $sth->execute( $feed, $guid ) or die "Could not insert ( $feed, $guid ) in table 'processed': $!\n";
-}
-
-sub _db_feeds_exists
-{
-  my $self  = shift;
-  my $url   = shift;
-
-  my $sth   = $self->{dbh}->prepare( "SELECT * FROM feeds WHERE url = ?;" );
-  $sth->execute( $url ) or die "Could not find $url in table 'feeds': $!\n";
-  my $row = $sth->fetchrow_hashref;
-  if( $row )
-  {
-    if( exists $row->{url} )
-    {
-      return 1;
-    }
-  }
-  return 0;
-}
-
-sub _db_processed_exists
-{
-  my $self  = shift;
-  my $feed  = shift;
-  my $guid  = shift;
-
-  my $sth   = $self->{dbh}->prepare( "SELECT * FROM processed WHERE feed = ? AND guid = ?;" );
-  $sth->execute( $feed, $guid ) or die "Could not find ($feed, $guid) in table 'processed': $!\n";
-  my $row = $sth->fetchrow_hashref;
-  if( $row )
-  {
-    if( exists $row->{url} )
-    {
-      return 1;
-    }
-  }
-  return 0;
-}
 
 =head1 NAME
 
